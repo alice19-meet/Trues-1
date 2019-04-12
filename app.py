e3cdd1d5726ac66a5e2ec08e956ab0b044cb8de6
aboud20-meet <aboud20@meet.mit.edu>
2019-04-12 08:11:43 +0300

    hi

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template, request, session, flash, url_for, redirect,send_from_directory
-import os
-import json, requests
+from flask import Flask, render_template, url_for, redirect, request
+from flask import session as login_session
+from database import *
 
 
 app = Flask(__name__)
+app.config["SECRETKEY"] = "change-this-letter"
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 
 @app.route('/')
-def home_page():
-	return render_template("homepage.html")
+def home():
+    return render_template("home.html")
 
 
+
+@app.route('/search', methods=["GET",'POST'])
+def search():
+    if request.method=='POST':
+        search=request.form["search"]
+        offers=query_by_name(search)
+        return render_template('offer.html')
+    if request.method=="GET":
+        return render_template('offer.html')
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
+
