58ef086875b199d7e747bc6f3938da4eee737ce4
myname21-meet <myname21@meet.mit.edu>
2019-07-31 05:22:35 -0400

    fixed

--- website/app.py
+++ website/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request
 from flask import session as login_session
-from database import *
+# from database import *
 
 
 app = Flask(__name__)
